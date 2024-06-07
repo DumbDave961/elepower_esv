c5cadb00e5cbdb7c2109e4e03b5ab4dfd3860524
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2024-06-06 20:16:26 -0500

    Update worldgen.lua
    Previous commit reverted.

--- elepower_dynamics/worldgen.lua
+++ elepower_dynamics/worldgen.lua
@@ -5,80 +5,43 @@ local epr = ele.external.ref
 --------------
 -- Worldgen --
 --------------
+-- Lead
 
-if minetest.get_modpath("technic_worldgen") then
-	return false
-else
-	-- Lead
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_lead",
-		wherein        = epr.stone,
-		clust_scarcity = 15 * 15 * 15,
-		clust_num_ores = 12,
-		clust_size     = 3,
-		y_max          = 31000,
-		y_min          = 1025,
-	})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_lead",
+	wherein        = epr.stone,
+	clust_scarcity = 15 * 15 * 15,
+	clust_num_ores = 12,
+	clust_size     = 3,
+	y_max          = 31000,
+	y_min          = 1025,
+})
 
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_lead",
-		wherein        = epr.stone,
-		clust_scarcity = 14 * 14 * 14,
-		clust_num_ores = 5,
-		clust_size     = 3,
-		y_max          = 0,
-		y_min          = -31000,
-	})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_lead",
+	wherein        = epr.stone,
+	clust_scarcity = 14 * 14 * 14,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	y_max          = 0,
+	y_min          = -31000,
+})
 
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_lead",
-		wherein        = epr.stone,
-		clust_scarcity = 10 * 10 * 10,
-		clust_num_ores = 5,
-		clust_size     = 3,
-		y_max          = -128,
-		y_min          = -31000,
-	})
-
--- Zinc
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_zinc",
-		wherein        = epr.stone,
-		clust_scarcity = 25 * 25 * 25,
-		clust_num_ores = 2,
-		clust_size     = 3,
-		y_max          = 31000,
-		y_min          = -31000,
-	})
-
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_zinc",
-		wherein        = epr.stone,
-		clust_scarcity = 20 * 20 * 20,
-		clust_num_ores = 5,
-		clust_size     = 3,
-		y_max          = 0,
-		y_min          = -31000,
-	})
-
-	minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "elepower_dynamics:stone_with_zinc",
-		wherein        = epr.stone,
-		clust_scarcity = 12 * 12 * 12,
-		clust_num_ores = 5,
-		clust_size     = 3,
-		y_max          = -256,
-		y_min          = -31000,
-	})
-end
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_lead",
+	wherein        = epr.stone,
+	clust_scarcity = 10 * 10 * 10,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	y_max          = -128,
+	y_min          = -31000,
+})
 
 -- Nickel
+
 minetest.register_ore({
 	ore_type       = "scatter",
 	ore            = "elepower_dynamics:stone_with_nickel",
@@ -155,5 +118,40 @@ minetest.register_ore({
 	clust_num_ores = 5,
 	clust_size     = 3,
 	y_max          = -12000,
+	y_min          = -31000,
+})
+
+-- Zinc
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_zinc",
+	wherein        = epr.stone,
+	clust_scarcity = 25 * 25 * 25,
+	clust_num_ores = 2,
+	clust_size     = 3,
+	y_max          = 31000,
+	y_min          = -31000,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_zinc",
+	wherein        = epr.stone,
+	clust_scarcity = 20 * 20 * 20,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	y_max          = 0,
+	y_min          = -31000,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "elepower_dynamics:stone_with_zinc",
+	wherein        = epr.stone,
+	clust_scarcity = 12 * 12 * 12,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	y_max          = -256,
 	y_min          = -31000,
 })
